1e86b6d7dc3deb227b5b38758e7380ed835c533f
ik012982i9 <a@a.a>
2024-05-27 13:03:48 +0200

    Hito 5.1 Mejorado

--- Hito5.1/src/main/java/org/example/CalculadoraLoca.java
+++ Hito5.1/src/main/java/org/example/CalculadoraLoca.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class CalculadoraLoca { public static void main(String[] args) {
 
     Scanner scanner = new Scanner(System.in);
-    double num1, num2;
+    int num1, num2;
     String operator;
 
     System.out.print("Introduce el primer número: ");
@@ -32,9 +32,10 @@ public class CalculadoraLoca { public static void main(String[] args) {
         System.out.println("El resultado es positivo.");
     }
 
+    scanner.close();
 }
 
-    public static double calculate(double num1, double num2, String operator) {
+    public static double calculate(int num1, int num2, String operator) {
 
         double result = 0;
 
@@ -46,15 +47,35 @@ public class CalculadoraLoca { public static void main(String[] args) {
                 result = num1 - num2;
                 break;
             case "*":
+                int[][] biderkatzeTaula = new int[10][10];
+                int lerroa = 0, zutabea = 0;
 
-                result = num1*num2;
+                for(int a = 0; a < biderkatzeTaula.length; a++)
+                {
+                    for(int b = 0; b < biderkatzeTaula[a].length; b++)
+                    {
+                        biderkatzeTaula[a][b] = lerroa * zutabea;
+                        zutabea = zutabea + 1;
+                    }
+                    lerroa = lerroa + 1;
+                    zutabea = 0;
+                }
+
+                result = biderkatzeTaula[num1][num2];
                 break;
             case "/":
-                if (num2!=0){
-                    result=num1/num2;
+                int zatiketa=0;
+
+                if (num2!=0) {
+                    while (num1 >= num2) {
+                        num1 -= num2;
+                        ++zatiketa;
+                    }
+                    result = zatiketa;
                 }else {
-                    System.out.println("División por cero no permitida");
+                    System.out.println("No se puede dividir por 0!");
                 }
+
                 break;
             default:
                 System.out.println("Operador no válido!");
